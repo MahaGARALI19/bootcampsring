f398d788c2cbd64ff10379f68271adbc8b486252
MahaGARALI19 <garalimaha9@gmail.com>
2022-02-21 20:06:06 +0100

    add css

--- src/main/java/com/sip/ams/controllers/HomeCrontroller.java
+++ src/main/java/com/sip/ams/controllers/HomeCrontroller.java
@@ -1,5 +1,7 @@
 package com.sip.ams.controllers;
 
+import java.util.ArrayList;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,6 +13,11 @@ public class HomeCrontroller {
 	public String info(Model model) {
 		String formation = "FullSlack";
 		model.addAttribute("workshop", formation);
+		ArrayList<String> names= new ArrayList<>();
+		names.add("OCA");
+		names.add("OCP");
+		names.add("Spring");
+		model.addAttribute("names", names);
 		return "home/info";
 	}
 	
