c5436d081ec6db9e8e3adc8faeb7f42aef389a53
MahaGARALI19 <garalimaha9@gmail.com>
2022-02-21 15:40:28 +0100

    passage de data vers la vue

--- src/main/java/com/sip/ams/controllers/HomeCrontroller.java
+++ src/main/java/com/sip/ams/controllers/HomeCrontroller.java
@@ -1,13 +1,16 @@
 package com.sip.ams.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeCrontroller {
 	
 	@RequestMapping("/info")
-	public String info() {
+	public String info(Model model) {
+		String formation = "FullSlack";
+		model.addAttribute("workshop", formation);
 		return "home/info";
 	}
 	
